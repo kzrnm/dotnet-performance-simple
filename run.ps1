a0934b47fb454624ccca63bc4b1c4bf245a6b25e
kzrnm <gengesa@gmail.com>
2024-05-12 21:30:07 +0900

    Fix run.ps1

--- run.ps1
+++ run.ps1
@@ -8,10 +8,15 @@ $env:Path = "$root\runtime\.dotnet\;${env:Path}"
 # ./build.cmd clr+libs+libs.tests -rc release -lc release
 
 Set-Location $PSScriptRoot
-dotnet run -c Release --filter $Filter --coreRun ('main', 'pr' | ForEach-Object { "$PSScriptRoot\coreruns\$_\corerun.exe" }) -m 
+
+$coreruns = @(
+    'main',
+    'pr'
+) | ForEach-Object { "$PSScriptRoot\coreruns\$_\corerun.exe" }
+dotnet run -c Release --filter $Filter --coreRun $coreruns -m -j short
 exit
 
-dotnet run -c Release --filter "*Parse*" -d --coreRun (
+$thresholds = @(
     20000,
     4932,
     3200,
@@ -19,4 +24,7 @@ dotnet run -c Release --filter "*Parse*" -d --coreRun (
     1233,
     616,
     308,
-    200 | ForEach-Object { "$root\coreruns\threshold-$_\corerun.exe" }) -j short -m
+    200
+)
+dotnet run -c Release --filter "*Parse*" -d --coreRun (
+    $thresholds | ForEach-Object { "$root\coreruns\threshold-$_\corerun.exe" }) -j short -m
